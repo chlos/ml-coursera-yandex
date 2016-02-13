644f5fd79b0ae82bf0dbfcd70845f137831fc717
chlos <baphonet@gmail.com>
2016-02-13 16:27:08 +0300

    week 3: lesson 1: assignment 1: support vectors
    solution added

--- w03/svm.py
+++ w03/svm.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python
+
+import pandas
+from sklearn.svm import SVC
+
+data = pandas.read_csv('svm-data.csv', header=None)
+y = data[0]
+X = data.iloc[0:, 1:]
+
+classifier = SVC(C=100000, kernel='linear', random_state=241)
+classifier.fit(X, y)
+print map(lambda x: x + 1, classifier.support_)
